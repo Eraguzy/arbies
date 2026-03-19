ecc71a26bb54954839e3443b3ca4916bf5ed9d71
Eraguzy <122539649+Eraguzy@users.noreply.github.com>
2026-03-19 14:12:49 +0100

    wip : design table for fundings; fix theme bug

--- app/funding/page.tsx
+++ app/funding/page.tsx
@@ -1,14 +1,65 @@
 "use client";
 
 import DexesSelector from "@/components/selectors/dexes";
+import { Table, TableCaption, TableHeader, TableRow, TableCell, TableHead, TableBody, } from "@/components/ui/table";
 import React from "react";
 
-export default function FundingPage() {
-	const [selected, setSelected] = React.useState<string[]>([]);
+export default function Funding() {
+	const [selected, setSelected] = React.useState<string[]>([]); // horizontal axis
+	const [compared, setCompared] = React.useState<string[]>([]); // vertical axis
+	React.useEffect(() => {
+		console.log("Selected DEXes:", selected);
+	}, [selected])
 
 	return (
 		<div>
-			<DexesSelector selected={selected} setSelected={setSelected} />
+			<div className="flex justify-between">
+				<div className="flex flex-row items-center gap-1 text-sm">
+					<DexesSelector
+						selected={selected}
+						setSelected={setSelected}
+						multiple
+						defaultSelected={true}
+					/>
+					to compare with
+					<DexesSelector
+						selected={compared}
+						setSelected={setCompared}
+						multiple
+						defaultSelected={false}
+						disabled={selected.length === 0}
+					/>
+				</div>
+				<h1 className="text-2xl font-bold">{compared.length > 0 && selected.length > 0 ? "Fundings Comparison" : "Fundings"}</h1>
+			</div>
+			<Table>
+				{selected.length !== 0 && <TableCaption>Click on a cell to view history (WIP)</TableCaption>}
+
+				<TableHeader>
+					<TableRow>
+						{
+							selected.length === 0 ? (
+								<TableHead className="flex justify-center">Select a DEX to view history</TableHead>
+							) : (
+								<>
+									<TableHead className="w-25">Asset</TableHead>
+									{selected.map((dex) => (
+										<TableHead key={dex} className="w-25">{dex}</TableHead>
+									))}
+								</>
+							)
+						}
+					</TableRow>
+				</TableHeader>
+
+				<TableBody>
+					{compared.map((dex) => (
+						<TableRow>
+							<TableCell key={dex} className="h-15">{dex}</TableCell>
+						</TableRow>
+					))}
+				</TableBody>
+			</Table>
 		</div>
 	);
 }
