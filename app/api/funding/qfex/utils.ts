4a547914f03525e80b362aea141ae7e17c0ab611
Eraguzy <122539649+Eraguzy@users.noreply.github.com>
2026-03-30 12:47:45 +0200

    add : qfex fundings

--- app/api/funding/qfex/utils.ts
+++ app/api/funding/qfex/utils.ts
@@ -5,5 +5,3 @@ export const QfexApiUrlEndpoints = {
   symbolsMetrics: '/symbols/metrics',
   fundingHsty: '/funding', // /funding/{symbol}
 };
-// WIP : i requested them to add a get funding feature
-// https://docs.qfex.com/api-reference/rest/market-data/symbol-metrics
